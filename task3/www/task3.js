cefda0659fedb5abad15d1b2981ecad9383db74e
Ершов Александр <eapbox@gmail.com>
2019-12-06 16:55:00 +0300

    task3_v2

--- task3/www/task3.js
+++ task3/www/task3.js
@@ -3,18 +3,22 @@
 document.querySelector('.workSpace').innerHTML = 'Loading task...';
 
 //Запрос на получение задания------------------------------------------------------
-fetch('/task3/variants')
-    .then(response => response.json())
-    .then(function(rez) {
-        let currentTask = `<h2 class="title">${rez.title}</h2>`;
-        for (var i = 0; i < rez.variants.length; i++) {
-            currentTask += `<button class="voteBtn" onclick="sendVote(this, ${rez.variants[i].id})">${rez.variants[i].name}</button>`;
-        }
-        document.querySelector('.workSpace').innerHTML = currentTask;
-    })
-    .catch(function(error) {
-        document.querySelector('.workSpace').innerHTML = '<h2 class="danger">Ошибка получения задания...</h2>';
-    })();
+function getTask() {
+    fetch('/task3/variants')
+        .then(response => response.json())
+        .then(function(rez) {
+            let currentTask = `<h2 class="title">${rez.title}</h2>`;
+            for (var i = 0; i < rez.variants.length; i++) {
+                currentTask += `<button class="voteBtn" onclick="sendVote(this, ${rez.variants[i].id})">${rez.variants[i].name}</button>`;
+            }
+            document.querySelector('.workSpace').innerHTML = currentTask;
+        })
+        .catch(function(error) {
+            document.querySelector('.workSpace').innerHTML = '<h2 class="danger">Ошибка получения задания...</h2>';
+        });
+};
+
+getTask();
 //----------------------------------------------------------------------------------
 
 //Отправить голос-------------------------------------------------------------------
